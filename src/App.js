26eb578331074090d843e45e894995ea4e09dddd
Latifa-Samad <samadlatifa07@gmail.com>
2022-08-16 18:15:56 +0000

    lesson on react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import ProfileCard from './components/ProfileCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    supper();
+
+    this.handleClick = this.handleClick.Click.bind(this)
+    
+    this.state = {
+      writers: {
+        loading: false,
+        list: []
+      }
+    }
+    
+    this.handleClick(){
+      this.setState({
+        writers: {
+          loading: true,
+        }
+      })
+
+      setTimeout(async () => {
+        let resp = await ('/writer.json');
+        let result = await resp. json()
+      })
+      
+      this.state({
+        writers: {
+          loading: false,
+          list: result
+        }
+      })
+
+    }
+
+  }
+
+  render() {
+    const {
+      writers: { loading, list }
+    } = this.state;
+  }
+
+  if(loading) {
+    return (
+      <div>
+        <h1>Writers profile</h1>
+        <div className='container'>
+          <div className='card action'>
+            <p className='infotext'> loading...</p>
+    
+
+          </div>
+    
+        </div>
+      </div>
+    
+    )
+  }
 }
 
-export default App;
+export default App
