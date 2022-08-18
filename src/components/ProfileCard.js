f25620dd7d05a4516ec9dd6fa03a0d6b322ca881
Latifa-Samad <110117204+Latifa-Samad@users.noreply.github.com>
2022-08-18 18:42:15 +0000

    hooks

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-function ProfileCaard({card}) {
+function ProfileCard({card}) {
   return (
-    <div className="greencard">
-    <h3>{card.firstname} {card.lastname}</h3>
-    <p>{card.email}</p>
-    <p>{card.phone}</p>
-      
+    <div className="greenCard">
+      <h3>{card.firstname} {card.lastname}</h3>
+      <p>{card.email}</p>
+      <p>{card.phone}</p>
     </div>
   )
 }
 
-export default ProfileCaard
+export default ProfileCard
--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -1,80 +1,56 @@
-import { useState } from "react"
-import React from 'react'
+import React, { useState } from 'react'
 
-function ProfileForm() {
-    const [profile, setProfile] =useState({
-       firstname: "",
-       lastname: "",
-       email: "",
-       phone: ""
-   });
-   
-   const handler=(event) => {
-    setProfile((prev) =>({
-        ...prev,
-        [event.target.name]: event.target.value
-    }));
+function ProfileForm({submit}) {
+    const [profile, setProfile] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        phone: ""
+    });
+
+    const handler = (event) => {
+        setProfile((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }));
+    }
+    const handleForm = (e) => {
+        e.preventDefault();
+        submit(profile)
 }
-const handleform=(e) =>{
-    e.preventDefault();
-    SubmitEvent(profile)
 
-}
-        
-
-     
-
-    
-
-    
-
-     
-   
 
   return (
-    <div className="fromcontainer">
-    <h3>profile from</h3>
-
-    <form>
-    <fieldset>
-    <legend>Bio data</legend>
-    <div className="names">
-    <lebal>First Name
-    <input name="firstname" value=
-    {profile.firstname} type="text"onChange={handler}/>
-
-    </lebal>
-
-    </div>
-    <div className="names">
-    <lebal>last Name
-    <input name="lastname" value=
-    {profile.lastname} type="text"onChange={handler}/>
-
-    </lebal>
-    </div>
-    
-    <div className="names">
-    <lebal>email
-    <input name="email" value=
-    {profile.email} type="text"onChange={handler}/>
-
-    </lebal>
-    </div>
-    
-    
-    <div className="names">
-    <lebal>phone
-    <input name="phone" value=
-    {profile.phone} type="text"onChange={handler}/>
-
-    </lebal>
-    </div>
-
-
-    </fieldset>
-    </form>
-
+      <div className="formContainer">
+          <h3>Profile Form</h3>
+          <form onSubmit={handleForm}>
+              <fieldset>
+                <legend>Bio data</legend>  
+                  <div className="names">
+                      <label>First Name
+                          <input name="firstname" value=
+                              {profile.firstname} type="text" onChange={handler}/>
+                      </label>
+                      <label>  Last Name
+                          <input name="lastname" value=
+                              {profile.lastname} type="text" onChange={handler}/>
+                      </label>
+                      
+               </div>
+                  <div className="names">
+                      <label>Email
+                          <input name="email" value=
+                              {profile.email} type="email" onChange={handler}/>
+                      </label>
+                      <label>  phone
+                          <input name="phone" value=
+                              {profile.phone} type="tel" onChange={handler}/>
+                      </label>
+                      
+               </div>
+              </fieldset>    
+<button className='form' type='submit'>add profile</button>
+</form>
     </div>
   )
 }
